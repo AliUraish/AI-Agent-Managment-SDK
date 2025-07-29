9918ca42ea344fdd101f6f4ea801511d5d54b30c
AliUraish <aliuraishmirani@gmail.com>
2025-07-29 13:58:44 +0500

    name packhage in setu.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="agent-operations-tracker",
+    name="AI-HR-Agent",
     version="1.0.0",
     packages=find_packages(),
     install_requires=[
@@ -9,12 +9,12 @@ setup(
         'aiohttp>=3.8.0',
         'python-dateutil>=2.8.0'
     ],
-    author="Your Name",
-    author_email="your.email@example.com",
-    description="A library for tracking AI Agent operations and metrics",
+    author="Ali Uraish",
+    author_email="aliuraish@gmail.com",
+    description="A Python SDK for tracking AI Agent operations and metrics",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/agent-operations-tracker",
+    url="https://github.com/AliUraish/AI-Agent-Managment-SDK",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
